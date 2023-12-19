5ed0f4beae076c410c1f86b58d5f206059f3e0e3
Lukas <lukascarvalho712@gmail.com>
2023-12-19 16:08:26 -0300

    começo da main e um pouco do estoque.js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.entrada.addEventListener('submit', leFormulario)
+
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
+const remove = document.querySelector("#Limpar")
+remove.addEventListener('click', limparEstoque);
