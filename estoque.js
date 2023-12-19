b26a08fb5a10d62daa235eda714a0898a12bcea2
Lukas <lukascarvalho712@gmail.com>
2023-12-19 16:11:52 -0300

    Adição de função no estoque.js

--- estoque.js
+++ estoque.js
@@ -6,3 +6,21 @@ let estoque = {
 export function getEstoque() {
     return structuredClone(estoque);
 }
+
+export function transacaoNoEstoque(Origem, Destino, fruta, quantidade) {
+    if (!estoque.joao) {
+        estoque.joao = [{ 'tipo': fruta, 'quantidade': 0 }];
+    }
+    if (!estoque.maria) {
+        estoque.maria = [{ 'tipo': fruta, 'quantidade': 0 }];
+    }
+    if (Origem === "pomar" && Destino !== "pomar") {
+        dePomarParaPessoa(Destino, quantidade, fruta);
+    }
+    else if (Destino === "pomar") {
+        dePomarParaPomar(Origem, quantidade, fruta);
+    }
+    else {
+        dePessoaParaPessoa(Origem, Destino, quantidade, fruta);
+    }
+}
